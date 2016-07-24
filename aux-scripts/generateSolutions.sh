f9a78073e3096bcdba6b0a9cf4c1df17062bc31d
Nirbhai <nsceh1@gmail.com>
2016-07-24 20:41:53 +0530

    downloaded solution files from online tstp repository

--- aux-scripts/generateSolutions.sh
+++ aux-scripts/generateSolutions.sh
@@ -4,5 +4,5 @@ cd problems
 for i in *
 do
 echo "$i"
-eprover --auto-schedule --definitional-cnf=24 -R --print-version --tstp-format -s --proof-object --cpu-limit=300 "$i" > "solutions/$i.soln"
+time eprover --auto-schedule --definitional-cnf=24 -R --print-version --tstp-format -s --proof-object --cpu-limit=300 "$i" > "solutions/$i.soln"
 done
