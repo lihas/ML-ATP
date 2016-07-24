a605db5214969f704d324ed51e1bec8fad68e067
vidhant <vidhant@gmail.com>
2016-07-24 20:52:12 +0530

    generate solution script changed. now running on 40 core server

--- aux-scripts/generateSolutions.sh
+++ aux-scripts/generateSolutions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PATH="$PATH:/home/singh/ML-ATP/E/PROVER/"
+PATH="$PATH:/home/singh/ML-ATP/E/PROVER/:/home/vishwajit/surykant/nb/ML-ATP/E/PROVER"
 cd problems
 for i in *
 do
 echo "$i"
-eprover --auto-schedule --definitional-cnf=24 -R --print-version --tstp-format -s --proof-object --cpu-limit=300 "$i" > "solutions/$i.soln"
+command time -f "$i %E" -a -o "time_log.txt" eprover --auto-schedule --definitional-cnf=24 -R --print-version --tstp-format -s --proof-object --cpu-limit=300 "$i" > "solutions/$i.soln"
 done
